ba89d89d5a6329d34b7d6aa447c0ee804f4a2be6
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2025-04-29 18:12:22 +0400

    reconstruction automatique des imports

--- src_app/cli/RebuildApp.php
+++ src_app/cli/RebuildApp.php
@@ -14,6 +14,9 @@ class RebuildApp extends Application {
   const ARGS = [
     "purpose" => "reconstruire la base de donnée des PVs de jury",
 
+    ["-c", "--verifix", "value" => true,
+      "help" => "vérifier la version, et reconstruire le cas échéant. implique --uploads --no-clean",
+    ],
     ["-u", "--uploads", "value" => true,
       "help" => "reconstruire depuis les fichiers du répertoire uploads",
     ],
@@ -22,17 +25,28 @@ class RebuildApp extends Application {
     ],
   ];
 
+  private bool $verifix = false;
   private bool $uploads = false;
   private bool $clean = false;
 
   function main() {
+    $check = $this->verifix;
+    $uploads = $this->uploads;
+    $clean = $this->clean;
+    if ($check) {
+      if (pvs::get_version() === pvs::EXPECTED_VERSION) return;
+      msg::info("Reconstruction des imports... Veuillez patienter");
+      $uploads = true;
+      $clean = false;
+    }
+
     $storageFile = pvs::storage_file();
-    if ($this->clean && file_exists($storageFile)) {
-      msg::info("clean storage");
+    if ($clean && file_exists($storageFile)) {
+      msg::info("suppression de la base de données");
       unlink($storageFile);
     }
 
-    if ($this->uploads) {
+    if ($uploads) {
       $files = sh::ls_pfiles(pvs::upload_file(null), "*.csv");
       $channel = pvs::channel();
       $channel->setRebuilder(true);
@@ -40,6 +54,7 @@ class RebuildApp extends Application {
         $filename = basename($file);
         msg::action("chargement de $filename", function() use ($channel, $file) {
           $channel->charge($file);
+          return true;
         });
       }
     } else {
@@ -49,8 +64,14 @@ class RebuildApp extends Application {
         $filename = basename($file);
         msg::action("chargement de $filename", function() use ($channel, $file) {
           $channel->charge($file);
+          return true;
         });
       }
+    }
+
+    if ($check) {
+      # mettre à jour la version
+      pvs::set_version(pvs::EXPECTED_VERSION);
     }
   }
 }
