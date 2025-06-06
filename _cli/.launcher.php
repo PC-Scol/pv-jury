b39d7f2a8c5fda9fcb27a9d3736f7117a35abe55
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2025-06-06 11:07:02 +0400

    corriger l'affichage des acquis

--- _cli/.launcher.php
+++ _cli/.launcher.php
@@ -1,9 +1,9 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 # Lancer une application en ligne de commande
 
 const NULIB_APP_app_params = [
-  "projdir" => __DIR__ . '/..',
+  "projdir" => __DIR__.'/..',
   "appcode" => \app\config\bootstrap::APPCODE,
 ];
 require __DIR__.'/../vendor/nulib/base/php/src/app/cli/include-launcher.php';
